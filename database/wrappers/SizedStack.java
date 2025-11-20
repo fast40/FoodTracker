aeddbec0c6666b0132e7b778a694becd9cc679ee
Ney <neycha@usc.edu>
2025-11-20 14:24:28 -0800

    SizedStack implemented

--- database/wrappers/SizedStack.java
+++ database/wrappers/SizedStack.java
@@ -1,5 +1,7 @@
 package db_helper;
 
+import java.util.Stack;
+
 public class SizedStack<T> extends Stack<T> {
 	protected int maxSize = 16;
 	public SizedStack(int size) {
@@ -7,6 +9,13 @@ public class SizedStack<T> extends Stack<T> {
 	}
 
 	public resize(int new_size) {
-		
+		this.maxSize = new_size;
 	}
+	@Override
+    public T push(T item) {
+        if (this.size() >= maxSize) {
+            this.remove(0);
+        }
+        return super.push(item);
+    }
 }
