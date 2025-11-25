79cb24e5533b8e414eda9b56de9e221543dd03d5
Ney <neycha@usc.edu>
2025-11-25 10:20:10 -0800

    Finished Listing the Nutrients in Java Wrapper

--- backend/src/main/java/database/wrappers/FoodItem.java
+++ backend/src/main/java/database/wrappers/FoodItem.java
@@ -1,9 +1,11 @@
 package database.wrappers;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FoodItem {
 	// TO DO - Finish this
-	public static enum NutrientTypes {
+	public static enum NutrientType {
 		CALORIES,
 		trans_fat, saturated_fat,
 		A, B1, B2, B3, B5, B6, B7, B9, B12, C, D, D2, D3, E, K,
@@ -13,9 +15,36 @@ public class FoodItem {
 		PROTEIN,
 		total_sugar, added_sugar, dietary_fiber;
 
-		public static final EnumSet<NutrientTypes> FAT = EnumSet.of(trans_fat, saturated_fat);
-		public static final EnumSet<NutrientTypes> VITAMINS = EnumSet.of(A, B1, B1, B2, B3, B5, B6, B7, B9, B12, C, D, D2, D3, E, K);
-		public static final EnumSet<NutrientTypes> MINERALS = EnumSet.of(calcium, iron, potassium);
-		public static final EnumSet<NutrientTypes> CARBOHYDRATES = EnumSet.of(total_sugar, added_sugar, dietary_fiber);
+		public static final EnumSet<NutrientType> FAT = EnumSet.of(trans_fat, saturated_fat);
+		public static final EnumSet<NutrientType> VITAMINS = EnumSet.of(A, B1, B1, B2, B3, B5, B6, B7, B9, B12, C, D, D2, D3, E, K);
+		public static final EnumSet<NutrientType> MINERALS = EnumSet.of(calcium, iron, potassium);
+		public static final EnumSet<NutrientType> CARBOHYDRATES = EnumSet.of(total_sugar, added_sugar, dietary_fiber);
 	}
+	// Member Variables
+	// I preface my private members with "m" out of habit from another class
+	// - Sid
+	private char[] mItemID = new char[32];
+	private char[] mOwnerID = new char[32];
+	private String mName;
+	private Map<NutrientType, Float> mNutrients = new HashMap<>();
+
+	// Functions
+	public FoodItem(String name, char[] foodID, char[] ownerID) {
+		mName = name;
+		mItemID = foodID;
+		mOwnerID = ownerID;
+	}
+	public void SetName(String newName){
+		mName = newName;
+	}
+	public String GetName() {return mName;}
+
+	// Helper Functions
+	private boolean PullNutrientData() {
+		// return true on successfully pulling data from database else false
+		// TO DO
+		return false;
+	}
+
+
 }
