47c3420cb139e643b99ee3fed1e39c8bae2841a7
Ney <neycha@usc.edu>
2025-11-28 15:36:34 -0800

    Added Enumeration Helper for Data Safety

--- backend/src/main/java/database/data_access/FoodHistoryDAO.java
+++ backend/src/main/java/database/data_access/FoodHistoryDAO.java
@@ -1,14 +1,33 @@
 package database.data_access;
 
+import database.helpers.Enumerations;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class FoodHistoryDAO {
     private static Integer PER_PAGE = 32;
-    public boolean AddToHistory(int UserID, int FoodID) {
-        // TO DO
-        return false;
+    // defaulting to breakfast, add an option of not included
+    public boolean AddToHistory(int user_id, int food_id) {return AddToHistory(user_id, food_id, 1, Enumerations.MealType.BREAKFAST); }
+    public boolean AddToHistory(int user_id, int food_id, int quantity, Enumerations.MealType meal_type) {
+        String query = "INSERT INTO food_logs (user_id, food_id, quantity, meal_type)" + "VALUES(?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, user_id);
+            stmt.setInt(2, food_id);
+            stmt.setInt(3, quantity);
+            stmt.setString(4, meal_type.GetName());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            // do something
+            // if (e.getErrorCode() == ___ ) {
+            //      return false;
+            // }
+            //throw e;
+            return false;
+        }
     }
 
     public List<Integer> GetHistory(Integer UserID, Integer page) throws SQLException {
---
+++ backend/src/main/java/database/helpers/Enumerations.java
@@ -0,0 +1,26 @@
+package database.helpers;
+
+public class Enumerations {
+    public enum MealType {
+        EMPTY("empty", 0),
+        BREAKFAST("breakfast", 1),
+        LUNCH("lunch", 2),
+        DINNER("dinner", 3),
+        SNACK("snack", 4);
+
+        private final String sql_string;
+        private final int sql_code;
+
+        MealType(String name, int code) {
+            this.sql_string = name;
+            this.sql_code = code;
+        }
+
+        public String GetName() {
+            return this.sql_string;
+        }
+        public int GetID() {
+            return this.sql_code;
+        }
+    }
+}
